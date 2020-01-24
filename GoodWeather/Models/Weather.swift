52a0dbb9bba76d09256488f40339bb441c3ed980
Alexander Smith <acsmith.dev@gmail.com>
2020-01-24 18:20:43 -0500

    add weather model

---
+++ GoodWeather/Models/Weather.swift
@@ -0,0 +1,20 @@
+//
+//  Weather.swift
+//  GoodWeather
+//
+//  Created by Alexander Smith on 1/24/20.
+//  Copyright © 2020 Alexander Smith. All rights reserved.
+//
+
+import Foundation
+
+// API JSON Data
+struct WeatherData: Decodable {
+  let main: Weather
+}
+
+// Decoded Weather Object
+struct Weather: Decodable {
+  var temp: Double?
+  var humidity: Double?
+}
